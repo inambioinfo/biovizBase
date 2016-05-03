f4756eec8bd9c200c586f16c9cd7ba32dd44aa90
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/biovizBase@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllGenerics.R
@@ -0,0 +1,9 @@
+setGeneric("addStepping",function(obj,...) standardGeneric("addStepping"))
+
+setGeneric("getFragLength", function(obj, ...) standardGeneric("getFragLength"))
+
+setGeneric("shrinkageFun", function(obj,...) standardGeneric("shrinkageFun"))
+
+setGeneric("maxGap", function(obj, ...) standardGeneric("maxGap"))
+
+setGeneric("spliceSummary", function(obj, model, ...) standardGeneric("spliceSummary"))
